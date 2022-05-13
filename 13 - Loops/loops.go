86a0ebe1a746157fd969f70a88daeb12b720833b
Clayton Santos <claytondesousasantos@gmail.com>
2022-05-13 06:37:03 -0300

    Atualizando exercício

--- 13 - Loops/loops.go
+++ 13 - Loops/loops.go
@@ -2,24 +2,26 @@
 
 package main
 
-//import "time"
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func main()  {
-//	i := 0
+func main() {
+	//	i := 0
 
-//	for i < 10 {
-//		i++
-//		fmt.Println("Incrementando i")
-//		time.Sleep(time.Second)		
-//	}
+	//	for i < 10 {
+	//		i++
+	//		fmt.Println("Incrementando i")
+	//		time.Sleep(time.Second)
+	//	}
 
-//	fmt.Println(i)
+	//	fmt.Println(i)
 
-//	for j := 0; j < 10; j++ {
-//		fmt.Println("Incrementando j", j)
-//		time.Sleep(time.Second)
-//	}
+	//	for j := 0; j < 10; j++ {
+	//		fmt.Println("Incrementando j", j)
+	//		time.Sleep(time.Second)
+	//	}
 
 	nomes := [3]string{"João", "Davi", "Lucas"}
 
@@ -32,8 +34,8 @@ func main()  {
 		fmt.Println(indice, string(letra))
 	}
 
-	usuario := map[string]string {
-		"nome": 	 "Leonardo",
+	usuario := map[string]string{
+		"nome":      "Leonardo",
 		"sobrenome": "Silva",
 	}
 
